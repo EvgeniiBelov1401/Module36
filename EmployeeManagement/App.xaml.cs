f66f4806d5363f0686a24f9d3c6388d31ae98593
Евгений <evgeniibelov1401@gmail.com>
2024-11-16 15:58:19 +0300

    Добавьте файлы проекта.

---
+++ EmployeeManagement/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.Repository;
+using EmployeeManagement.ViewModels;
+using EmployeeManagement.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Unity;
+
+namespace EmployeeManagement
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            IUnityContainer container = new UnityContainer();
+
+            container.RegisterType<ILogger, Logger>();
+            container.RegisterType<IEmployeeViewModel, EmployeeViewModel>();
+            container.RegisterType<IEmployeesViewModel, EmployeesViewModel>();
+            container.RegisterType<IEmployeeRepository, EmployeeRepository>();
+
+            container.Resolve<EmployessView>().Show();
+        }
+    }
+}
---
+++ EmployeeManagement/Views/EmployessView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EmployeeManagement.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для EmployessView.xaml
+    /// </summary>
+    public partial class EmployessView : Window
+    {
+        public EmployessView()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_PrevieMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var item = (sender as ListView).SelectedItem;
+
+            if (item is null)
+            {
+                return;
+            }
+            var emploee = item as Employee;
+            MessageBox.Show(
+                $"Сотрудник: {emploee.LastName} {emploee.FirstName}\n" +
+                $"Возраст: {emploee.Age}\n" +
+                $"Должность: {emploee.Position}\n" +
+                $"Копания: {emploee.CompanyName} ({emploee.CityName})");
+        }
+
+    }
+}
